3218bdf83f37541772fb727f07ced2bec7061d5d
Sarina0 <seyedehsarina.mousavi@student.vcc.ca>
2023-03-21 21:07:24 -0700

    Main

---
+++ HashTableQuadraticProbing.java
@@ -0,0 +1,118 @@
+package Assignment2;
+
+import javax.xml.crypto.Data;
+
+public class HashTableQuadraticProbing{
+    private int currentSize;
+    private static int maxSize;       
+    private static Integer[] keys;   
+    private static String[] vals;   
+   
+
+    public HashTableQuadraticProbing(int capacity){
+
+        currentSize = 0;
+        maxSize = capacity;
+        keys = new Integer[maxSize];
+        vals = new String[maxSize];
+    }
+
+    private static int hash(Integer key) 
+    {
+        return key.hashCode() % maxSize;
+    } 
+    public int size() 
+    {
+        return currentSize;
+    }
+    public boolean isEmpty() 
+    {
+        return size() == 0;
+    }
+    public boolean contains(Integer key) 
+    {
+        return get(key) !=  null;
+    }
+    public void add(Integer key, String val) 
+    {                
+        int tmp = hash(key);
+        int i = tmp, h = 1;
+        do
+        {
+            if (keys[i] == null)
+            {
+                keys[i] = key;
+                vals[i] = val;
+                currentSize++;
+                return;
+            }
+            if (keys[i].equals(key)) 
+            { 
+                vals[i] = val; 
+                return; 
+            }            
+            i = (i + h * h++) % maxSize;            
+        } while (i != tmp);       
+    }
+    public static String get(Integer key) 
+    {
+        int i = hash(key), h = 1;
+        while (keys[i] != null)
+        {
+            if (keys[i] == key)
+                return vals[i];
+            i = (i + h * h++) % maxSize;
+           // System.out.println("i "+ i);
+        }            
+        return null;
+    }
+
+    public void remove(Integer key) 
+    {
+        if (!contains(key)) 
+            return;
+ 
+        /** find position key and delete **/
+        int i = hash(key), h = 1;
+        while (!key.equals(keys[i])) 
+            i = (i + h * h++) % maxSize;        
+        keys[i] = null;
+        vals[i] = null;
+ 
+        /** rehash all keys **/        
+        for (i = (i + h * h++) % maxSize; keys[i] != null; i = (i + h * h++) % maxSize)
+        {
+            Integer tmp1 = keys[i];
+            String tmp2 = vals[i];
+            keys[i] = null;
+            vals[i] = null;
+            currentSize--;  
+            add(tmp1, tmp2);            
+        }
+        currentSize--;        
+    }
+    public static void main(String[] args){
+
+        HashTableQuadraticProbing table = new HashTableQuadraticProbing(10);
+    
+        System.out.println("This is Seperate Chaining Tests");
+        System.out.println("'");
+        table.add(3,"crab");
+        table.add(11,"dolphin");
+        table.add(7,"koala");
+        table.add(4,"giraf");
+        System.out.println(table.contains(7)); // expected true
+        System.out.println(table.size());  // expected 4
+        System.out.println(get(3));  // expected crab
+        System.out.println(get(31));   //expected null
+        System.out.println(get(4));    //expected giraf
+        table.remove(3);
+        System.out.println(table.size()); // expected 3
+        table.remove(44);
+    
+        System.out.println(table.size()); // expected 3 because 44 does not exsit
+        table.remove(7);
+        System.out.println(table.size()); // expected 2 
+
+    }
+}
---
+++ HashTableSeperateChaining.java
@@ -0,0 +1,133 @@
+package Assignment2;
+
+import java.util.HashMap;
+
+public class HashTableSeperateChaining{
+
+    private static HashNode[] buckets;
+    private static int numOfBuckets;
+    private int size;
+   
+    public HashTableSeperateChaining(){
+        this(10); //defualt capacity
+    }
+    public HashTableSeperateChaining(int capacity){
+        this.numOfBuckets = capacity;
+        this.buckets = new HashNode[numOfBuckets];
+        this.size = 0;
+    }
+
+
+    private class HashNode {
+        private Integer key;
+        private String value;
+        private HashNode next;
+    
+        public HashNode(Integer key, String value){
+            this.key = key;
+            this.value = value;
+        }
+}
+public int size(){
+    return size;
+}
+
+public boolean isEmpty(){
+    return size == 0;
+
+}
+
+public void add(Integer key, String value){
+if(key == null || value == null){
+    throw new IllegalArgumentException("Key or value is null");
+}
+int bucketIndex = getBucketIndex(key);
+HashNode head = buckets[bucketIndex];
+
+while(head!= null){
+    if(head.key.equals(key)){
+        head.value = value;
+        return;
+    }
+    head = head.next;
+}
+size++;
+head = buckets[bucketIndex];
+HashNode node = new HashNode(key, value);
+node.next = head;
+buckets[bucketIndex] = node;
+}
+private static int getBucketIndex(Integer key){
+    return key % numOfBuckets;
+}
+
+public static String get(Integer key){
+    if(key == null){
+        throw new IllegalArgumentException("key is null!");
+    }
+    int bucketIndex = getBucketIndex(key);
+    HashNode head = buckets[bucketIndex];
+    while(head!= null){
+        if(head.key.equals(key)){
+          return head.value;
+        }
+        head = head.next;
+    }
+return null;
+}
+
+public String remove(Integer key){
+    if(key == null){
+        throw new IllegalArgumentException("key is null!");
+    }
+
+    int bucketIndex = getBucketIndex(key);
+    HashNode head = buckets[bucketIndex];
+    HashNode previous = null;
+
+    while(head!= null){
+        if(head.key.equals(key)){
+            break;
+        }
+        previous = head;
+        head = head.next;
+    }
+    if(head == null){
+        return null;
+    }
+    size--;
+    if(previous!= null){
+        
+    }else{
+        buckets[bucketIndex] = head.next;
+    }
+    return head.value;
+}
+
+public static void main(String[] args){
+
+    HashTableSeperateChaining table = new HashTableSeperateChaining(10);
+
+    System.out.println("This is Seperate Chaining Tests");
+    System.out.println("'");
+    table.add(3,"cat");
+    table.add(11,"dogs");
+    table.add(7,"koala");
+    table.add(4,"ants");
+    System.out.println(table.size());  // expected 4
+    System.out.println(get(3));  // expected cat
+    System.out.println(get(31));   //expected null
+    System.out.println(get(4));    //expected ants
+    table.remove(3);
+    System.out.println(table.size()); // expected 3
+    table.remove(44);
+    System.out.println(table.size()); // expected 3 because 44 does not exsit
+    table.remove(7);
+    System.out.println(table.size()); // expected 2 
+    System.out.println(table.isEmpty()); // expected false 
+    table.remove(11);
+    table.remove(4);
+    System.out.println(table.size());  // expected 0
+    System.out.println(table.isEmpty());   //expected true
+}
+}
